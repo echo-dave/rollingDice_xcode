5607b6ca42755a675da35fcf9501d013a7e0cb66
Dave May <echo.dave@protonmail.com>
2021-10-28 00:36:48 -0400

    Randomize dice on touch of roll btn

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var diceView1: UIImageView!
+    @IBOutlet weak var diceView2: UIImageView!
+    
+ 
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//    }
+
+    @IBAction func rollDice(_ sender: UIButton) {
+        let dice = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+//        func randomDice() -> Int { return Int.random(in: 0...5) }
+        diceView1.image = dice.randomElement()
+        diceView2.image = dice.randomElement()
     }
-
-
+    
 }
 

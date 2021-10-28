79970b15b6ae29126810f0328653e1dfe0f841f6
Dave May <echo.dave@protonmail.com>
2021-10-28 14:24:54 -0400

    Some code cleanup

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -12,12 +12,6 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var diceView1: UIImageView!
     @IBOutlet weak var diceView2: UIImageView!
-    
- 
-    
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//    }
 
     @IBAction func rollDice(_ sender: UIButton) {
         let dice = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
@@ -25,6 +19,5 @@ class ViewController: UIViewController {
         diceView1.image = dice.randomElement()
         diceView2.image = dice.randomElement()
     }
-    
 }
 
